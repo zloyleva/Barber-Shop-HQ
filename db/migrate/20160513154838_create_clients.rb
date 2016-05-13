227397e9b9492731ad7027c8fc597b9093cc6576
Levchenko Oleg <zloyleva@gmail.com>
2016-05-13 21:14:09 +0300

    Add Clients migration

---
+++ db/migrate/20160513154838_create_clients.rb
@@ -0,0 +1,13 @@
+class CreateClients < ActiveRecord::Migration
+  def change
+  	create_table :client do |t|
+  		t.text :name
+  		t.text :phone
+  		t.text :datestamp
+  		t.text :barber
+  		t.text :color
+
+  		t.timestamp
+  	end
+  end
+end
