0381396a6b94ca823e5bc0c215b8a525fe011baa
Levchenko Oleg <zloyleva@gmail.com>
2016-05-15 13:43:20 +0300

    Save client information - cool way

--- app.rb
+++ app.rb
@@ -28,23 +28,8 @@ end
 
 post '/visit' do
 
-	@username = params[:username]
-	@phone = params[:phone]
-	@datetime = params[:datetime]
-	@barber = params[:barber]
-	@color = params[:color]
-
-	hh = {:username => 'Enter your name', :phone => 'Enter your phone', :datetime => 'Enter date and time'}
-
-	@error = hh.select{|key, _| params[key] == ''}.values.join(', ')
-
-	if @error != ''
-		
-		return erb :visit
-	end
-
-	new_client = Client.new :name => "#{@username}", :phone => "#{@phone}", :datestamp => "#{@datetime}", :barber => "#{@barber}", :color => "#{@color}"
+	new_client = Client.new params[:client_param]
 	new_client.save
 
-	erb "Thanks #{@username}! Our meneger will call you."
+	erb "Thanks! Our meneger will call you."
 end
