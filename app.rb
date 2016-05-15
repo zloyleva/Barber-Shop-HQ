7fd5cc857688d4a9264326ca945a142797d5d76d
Levchenko Oleg <zloyleva@gmail.com>
2016-05-15 15:29:43 +0300

    Add validation, but don`t show error

--- app.rb
+++ app.rb
@@ -7,6 +7,9 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
+	validates :name, {presence: true}
+	validates :phone, {presence: true}
+	validates :datestamp, {presence: true}
 end
 
 class Barber < ActiveRecord::Base
