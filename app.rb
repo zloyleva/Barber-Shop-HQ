962733cb044b4ee35790af89ea738a30b40ed0f0
Levchenko Oleg <zloyleva@gmail.com>
2016-05-15 12:48:37 +0300

    New day commit

--- app.rb
+++ app.rb
@@ -12,7 +12,28 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.all
+end
+
 get '/' do
 	@barbers = Barber.all
 	erb :index
+end
+
+get '/visit' do
+		
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	
+	erb :visit
 end
